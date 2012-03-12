53dd009250e3001994824f30b643f2cadda2eaeb
Simen Kind Gulbrandsen <simgul@gmail.com>
2012-03-12 13:54:08 +0100

    nomoreconflictsplease

--- DiBi-clustering/DiBi-clustering/src/createArff/CreateArff.java
+++ DiBi-clustering/DiBi-clustering/src/createArff/CreateArff.java
@@ -1,5 +1,15 @@
 package createArff;
 
+import weka.core.Attribute;
+import weka.core.FastVector;
+import weka.core.Instance;
+import weka.core.Instances;
+
 public class CreateArff {
 
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+	
+	
 }
